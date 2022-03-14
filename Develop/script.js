5bea42a98fdb81faa0a0b210737bbda8bcac1e44
Your Name <lubezkibernard@Lubezkis-MacBook-Pro.local>
2022-03-13 20:01:35 -0400

    basic criterias

--- Develop/script.js
+++ Develop/script.js
@@ -15,3 +15,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 8 but less than 128.");
+  if (passwordLength < 8) {
+    alert("Password must be at least 8 characters!");
+    return;
+  }
+  if (passwordLength > 128) {
+    alert("Password must be less than 128 characters!");
+    return;
+  }
+
+  var numbers = confirm("Do you want numbers in your password?");
+
+  var lowerCases = confirm("Do you want lowercases in your password?");
+
+  var upperCases = confirm("Do you want uppercases in your password?");
+
+  var special = confirm("Do you want special characters in your password?");
+  
+};
+
+var specialChar = "!@#$%^&*(){}[]:;<,>.?/"
+var lowercaseChar = "qwertyuiopasdfghjklzxcvbnm"
+var uppercaseChar = "QWERTYUIOPASDFGHJKLZXCVBNM"
+var numberChar = "0123456789"
